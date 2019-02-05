cf4b98806b94c1aec182c345731ba89d36b63cba
sdixit03 <40880273+sdixit03@users.noreply.github.com>
2019-02-05 10:33:18 +0530

    Update oddevn.py

--- oddevn.py
+++ oddevn.py
@@ -1,6 +1,9 @@
 print("Enter a number: ")
 num= int(input())
-if num/2==0:
-    print("EVEN")
+if num>0:
+    if num/2==0:
+        print("EVEN")
+    else:
+        print("ODD")
 else:
-    print("ODD")
+    print("INVALID")
